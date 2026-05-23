eef483d6d8bbe35f395db4baddc2921b9998a4cf
Prashant Jondhale <jondhaleprashant@gmail.com>
2026-05-23 11:20:43 +0530

    Initial commit

---
+++ app/src/main/java/com/swarajya/smartexpensetracker/base/presentation/SmartTrackerApplication.kt
@@ -0,0 +1,8 @@
+package com.swarajya.smartexpensetracker.base.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SmartTrackerApplication : Application() {
+}
